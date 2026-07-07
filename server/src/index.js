30ab07c16f8ab7cb0ba3c49cb7a4eb5e10c8ab58
JosemariaNA <napanjosemaria@gmail.com>
2026-07-06 20:57:03 -0500

    Agregado directo el encabezado

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
  
 const allowedOrigins = [
   process.env.FRONTEND_URL, 
+  'https://inversion-segura.vercel.app',
   'http://localhost:5173', 
   'http://localhost:3000'
 ];
